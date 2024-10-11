78bcc55cc7cedeb311565faa1b3cd20dcf3cb3d6
isreal575 <adeniranisreal852@gmail.com>
2024-10-11 22:07:22 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+
+  console.log(Math.floor(window.innerWidth / 270));
+});
+
+//TOGGLE
+
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
+);
+
+ball.addEventListener("click", () => {
+  items.forEach((item) => {
+    item.classList.toggle("active");
+  });
+  ball.classList.toggle("active");
+});
+function playVideo(button) {
+    const video = button.previousElementSibling; // Get the video element
+    if (video.paused) {
+        video.play();
+        button.innerText = "Pause"; // Change button text
+    } else {
+        video.pause();
+        button.innerText = "Watch"; // Change button text back
+    }
+}
